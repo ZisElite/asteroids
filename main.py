e152c8c3423c729b6cbef8b386e23ee5ef5fe13a
ZisElite <zisispsalidas.3d@gmail.com>
2025-01-15 15:05:58 +0200

    Ristrict to 60 fps

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     pygame.init()
     print(pygame.get_init())
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
